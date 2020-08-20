fbcfdac03b889c438e48e996391bc1ae770fed4c
nayirong <yirong.na@gmail.com>
2020-08-21 00:37:21 +0800

    added back test code, disable test from building

--- SmartWalletUITests/SmartWalletUITests.swift
+++ SmartWalletUITests/SmartWalletUITests.swift
@@ -24,40 +24,40 @@ class SmartWalletUITests: XCTestCase {
 		super.tearDown()
 	}
 
-//    func testTakingSnapshot() {
-//		let app = XCUIApplication()
-//		sleep(30)
-//
-//		let startButton = app.buttons["Start Now!"]
-//		if startButton.exists {
-//			snapshot("1- Splash", timeWaitingForIdle: 10)
-//			startButton.tap()
-//		}
-//
-//		snapshot("3- Records", timeWaitingForIdle: 10)
-//		app.tables.cells.element(boundBy: 0).tap()
-//		app.buttons["Edit"].tap()
-//		snapshot("2- Add Edit Record", timeWaitingForIdle: 10)
-//		app.navigationBars["Add Record"].buttons["Record"].tap()
-//		app.navigationBars["Record"].buttons["Records"].tap()
-//
-//		let tabBarsQuery = app.tabBars
-//
-//		tabBarsQuery.buttons["Budget"].tap()
-//		snapshot("4- Budget", timeWaitingForIdle: 10)
-//
-//		tabBarsQuery.buttons["Settings"].tap()
-//
-//		let tablesQuery = app.tables
-//		tablesQuery.staticTexts["Categories"].tap()
-//		snapshot("7- Categories", timeWaitingForIdle: 10)
-//
-//		app.navigationBars["Categories"].buttons["Settings"].tap()
-//
-//		tabBarsQuery.buttons["Dashboard"].tap()
-//		snapshot("5- Dashboard", timeWaitingForIdle: 10)
-//
-//		app.swipeUp()
-//		snapshot("6- Dashboard Bottom", timeWaitingForIdle: 10)
-//    }
+    func testTakingSnapshot() {
+		let app = XCUIApplication()
+		sleep(30)
+
+		let startButton = app.buttons["Start Now!"]
+		if startButton.exists {
+			snapshot("1- Splash", timeWaitingForIdle: 10)
+			startButton.tap()
+		}
+
+		snapshot("3- Records", timeWaitingForIdle: 10)
+		app.tables.cells.element(boundBy: 0).tap()
+		app.buttons["Edit"].tap()
+		snapshot("2- Add Edit Record", timeWaitingForIdle: 10)
+		app.navigationBars["Add Record"].buttons["Record"].tap()
+		app.navigationBars["Record"].buttons["Records"].tap()
+
+		let tabBarsQuery = app.tabBars
+
+		tabBarsQuery.buttons["Budget"].tap()
+		snapshot("4- Budget", timeWaitingForIdle: 10)
+
+		tabBarsQuery.buttons["Settings"].tap()
+
+		let tablesQuery = app.tables
+		tablesQuery.staticTexts["Categories"].tap()
+		snapshot("7- Categories", timeWaitingForIdle: 10)
+
+		app.navigationBars["Categories"].buttons["Settings"].tap()
+
+		tabBarsQuery.buttons["Dashboard"].tap()
+		snapshot("5- Dashboard", timeWaitingForIdle: 10)
+
+		app.swipeUp()
+		snapshot("6- Dashboard Bottom", timeWaitingForIdle: 10)
+    }
 }
